6176feabe2f9932f10b2725d3cc976af39714297
Michael Stevan Lapandio <michaellapandio04@gmail.com>
2023-04-12 06:17:37 +0700

    done learning at 12/4/2023

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import ProfileCard from './ProfileCard';
 
 function App() {
     return (
-        <div>Personal Digital Assistants
+        <div>
+            <div>Personal Digital Assistants</div>
 
-
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        </div>
+            <ProfileCard title="Alexa" handle="@alexa99" />
+            <ProfileCard title="Cortana" handle="@cortana32" />
+            <ProfileCard title="Siri" handle="@siri01" />
+        </div>  
     );
 }
 
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,15 @@
-function ProfileCard() {
-    return <div>Profile Card!!!</div>;
+function ProfileCard({ title, handle }) { //optional using ProfileCard(props)
+    // const title = props.title;
+    // const handle = props.handle;
+    // one handled line
+    // const { title, handle } = props;
+
+    return (
+        <div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </div>
+    );
 }
 
 export default ProfileCard;
